ce2883f17ad96954f91e5b9d67cb637a120c225f
Alejandro Vasquez <alejavzqz@outlook.com>
2020-08-24 06:40:42 -0600

    application: make sidekiq queue adapter

--- config/application.rb
+++ config/application.rb
@@ -22,8 +22,7 @@ module FlowspaceBakery
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.active_job.queue_adapter = :sidekiq
+
   end
 end
